6238b48e09bd9e370e827003f9e2a6fde1ddbe51
Elijah72 <elijahkanyia72@gmail.com>
2023-12-11 22:41:01 +0300

    staticroot changes

--- digicare/settings.py
+++ digicare/settings.py
@@ -115,14 +115,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATIC_URL = 'static/'
-if DEBUG:
-        STATICFILES_DIRS = [
-            os.path.join(BASE_DIR, 'static')
-       ]
-else:
-        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # sendin emails
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
