0efbeea06c8fe927519097e9003606621b0a3ca6
Elijah72 <elijahkanyia72@gmail.com>
2023-12-11 22:31:31 +0300

    static should not contain content of staticfilesdirs

--- digicare/settings.py
+++ digicare/settings.py
@@ -121,7 +121,7 @@ if DEBUG:
         STATICFILES_DIRS = [
             os.path.join(BASE_DIR, 'static')
        ]
-    else:
+else:
         STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # sendin emails
