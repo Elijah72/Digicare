a544a7d26b825757258ad361a504f9f04fda2a88
Elijah72 <elijahkanyia72@gmail.com>
2023-12-13 01:40:19 +0300

    whitenoise

--- digicare/settings.py
+++ digicare/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'care',
     'django_daraja',
-    'import_export'
+    'import_export',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,12 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 # sendin emails
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
